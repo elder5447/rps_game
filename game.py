73fa52de3040d53be343a072b0718fab666e4d0b
elder5447 <114049847+elder5447@users.noreply.github.com>
2022-09-21 09:08:24 +0300

    Update game.py

--- game.py
+++ game.py
@@ -8,10 +8,10 @@ computer_turn = random.choice(possible_actions)
 if computer_turn == human_turn:
     print("niča\n"*10)
 elif human_turn == "rock" and computer_turn == "scissors":
-  print("winner wineer chicken dinner!!!\n"*100)
+  print("winner wineer chicken dinner!!!\n"*10)
 elif human_turn == "paper" and computer_turn == "rock":
-  print("winner wineer chicken dinner!!!\n"*100)
+  print("winner wineer chicken dinner!!!\n"*10)
 elif human_turn == "scissors" and computer_turn == "paper":
-  print("winner wineer chicken dinner!!!\n"*100)
+  print("winner wineer chicken dinner!!!\n"*10)
 else:
-    print("Žeņas vaina\n"*38)
+    print("You loose\n"*10)
